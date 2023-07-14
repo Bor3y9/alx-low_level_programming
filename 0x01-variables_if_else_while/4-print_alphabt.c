e1deeca9ef53c76e4d290e047268f7c35c522182
Bor3y9 <yousef161247@eng.psu.edu.eg>
2023-07-14 05:38:49 -0700

    C programming

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include<stdlib.h>
+#include<time.h>
+#include<stdio.h>
+/**
+ * main - checking for negativity
+ *
+ * Return:0(sucess)
+ */
+int main(void)
+{
+
+	char  ch;
+	for(ch = 'a'; ch <= 'z'; ch++)
+	{
+		if(ch !='q' && ch!='e')
+		{
+			putchar(ch);
+		}
+	}
+	putchar(10);
+
+	return(0);
+}
