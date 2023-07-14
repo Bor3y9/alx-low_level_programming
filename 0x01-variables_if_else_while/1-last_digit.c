56cac50d96c58e1ff8ce3088cd75981ed86f95ca
Bor3y9 <yousef161247@eng.psu.edu.eg>
2023-07-14 05:25:46 -0700

    C programming

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,6 +16,7 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	x = n % 10;
+
 	printf("Last digit of %d is %d ", n, x);
 
 	if (x > 5)
@@ -32,6 +33,7 @@ int main(void)
 	{
 		printf("and is less than 6 and not 0");
 	}
-printf("\n");
-return(0);
+
+	printf("\n");
+	return (0);
 }
