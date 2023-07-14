046bc55a8559d929fff030296b04e897b16c1802
Bor3y9 <yousef161247@eng.psu.edu.eg>
2023-07-14 05:34:00 -0700

    C programming

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,14 +9,12 @@
 int main(void)
 {
 	int ch;
-	for(ch = 97; ch <= 122; ch++){
-putchar(ch);
-	}
-	for (ch = 65; ch <= 90; ch++)
+
+	for (ch = 97; ch <= 122; ch++)
 	{
 		putchar(ch);
 	}
+	putchar(10); /* this is an ASCII code for new line */
 
-	purchar(10);
-	return(0);
+	return (0);
 }
