23154ca83d3da8e98ac5513d310bad891880aa57
Bor3y9 <yousef161247@eng.psu.edu.eg>
2023-07-14 05:47:17 -0700

    C programming

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include<stdlib.h>
+#include<time.h>
+#include<stdio.h>
+/**
+ * main - checking for negativity
+ *
+ * Return:0(sucess)
+ */
+int main(void)
+{
+
+	int ch;
+	for(ch = 48; ch <= 57; ch++)
+	{
+		putchar(ch);
+	}
+	putchar(10);
+
+	return(0);
+}
