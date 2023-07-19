cd708322a5bb96c2eb613c912e68ad537e7f2a7a
Bor3y9 <yousef161247@eng.psu.edu.eg>
2023-07-19 10:49:47 -0700

    hello

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,24 +9,16 @@
  */
 int print_sign(int n)
 {
-	if (n > 0)
-	{
-		putchar('+');
-		return (1);
-	}
-	else if (n == 0)
-	{
-		putchar('0');
-		return (0);
-	}
-	else if (n < 0)
-	{
-		putchar('-');
-		return (-1);
-	}
-	else
-	{
-		putchar('-');
-		return ('/');
-	}
+if (n > 0)
+{
+    return (+1);
+}
+else if (n == 0)
+{
+return (00);
+}
+else if (n < 0)
+{
+return (-1);
+}
 }
