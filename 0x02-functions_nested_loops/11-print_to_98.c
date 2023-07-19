17ca63b3a000cd87ef59d0ffaecf65043c799dc8
Bor3y9 <yousef161247@eng.psu.edu.eg>
2023-07-19 12:56:09 -0700

    hello

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,5 +1,6 @@
-#include "main.h"
-
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
 /**
  *print_to_98 - Write a function that prints all natural numbers
  * from n to 98, followed by a new line.
