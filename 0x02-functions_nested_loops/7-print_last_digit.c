babeb92dd8c1f2574d79de75d7b842fe1b2a09d9
Bor3y9 <yousef161247@eng.psu.edu.eg>
2023-07-19 10:55:16 -0700

    hello

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * print_last_digit - print_last_digit
+ * @n: the integer to be converted.
+ * Return: the value of the last digit
+ */
+
+int print_last_digit(int n)
+{
+n = n % 10;
+return n;
+}
