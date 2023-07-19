c6c2cb5a6fc4a31164f9670c2eddb2e0f80170de
Bor3y9 <yousef161247@eng.psu.edu.eg>
2023-07-19 08:43:17 -0700

    hello

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,15 +2,21 @@
 #include <time.h>
 #include <stdio.h>
 
+/**
+ *main - assign a random number to the variable n each time it is executed
+ *and print the last digit of the number stored in the variable n
+ *Return: Always 0 (Success)
+ */
 void print_alphabet(void)
 {
 
-    int i;
+int i;
 
-    for (i = 'a'; i <= 'z'; i++)
-    {
+for (i = 'a'; i <= 'z'; i++)
+{
 
-        putchar(i);
-    }
-    putchar('\n');
+putchar(i);
 }
+putchar('\n');
+}
+
