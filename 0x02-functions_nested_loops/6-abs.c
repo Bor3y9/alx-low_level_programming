ceb26cb690c679c0dbd0bd6c79705b62c38e69a2
Bor3y9 <yousef161247@eng.psu.edu.eg>
2023-07-19 10:52:56 -0700

    hello

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * _abs - calculate the absolute value of the number from zero.
+ * @n: the integer to be converted.
+ * Return: absolute value of the input number.
+ */
+
+int _abs(int n)
+{
+if (n < 0)
+{
+n = n * -1;
+return (n);
+}
+else
+{
+return (n);
+}
+}
