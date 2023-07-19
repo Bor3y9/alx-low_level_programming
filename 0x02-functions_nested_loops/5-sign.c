08ff85298342929ce90014384745d44b4ccabdd9
Bor3y9 <yousef161247@eng.psu.edu.eg>
2023-07-19 13:00:52 -0700

    hello

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,24 +1,29 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 /**
- * print_sign - Determines whether an integer is positive, negative or zero.
- * @n: the number to be checked.
- * Return: 1 if positive, 0 if zero, -1 if negative, / if not a digit.
+ * print_sign - Compare the variable N
+ * @n: If is mayor than 0 show return 1 else 0
+ *
+ * Return: Always 0.
  */
+
 int print_sign(int n)
 {
-if (n > 0)
-{
-    return (+1);
-}
-else if (n == 0)
-{
-return (00);
-}
-else if (n < 0)
-{
-return (-1);
-}
+
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
 }
