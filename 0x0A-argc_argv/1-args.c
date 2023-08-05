6e9a9f283cc511f8dffa5de714739955ef4b4abf
Bor3y9 <yousef161247@eng.psu.edu.eg>
2023-08-04 18:36:30 -0700

    args,argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - program should print a number, followed by a new line
+ * @argc: number of arguments
+ * @argv: array with the arguments
+ *
+ * Return: always 0
+ **/
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	printf("%s\n", argc);
+	return (0);
+}
