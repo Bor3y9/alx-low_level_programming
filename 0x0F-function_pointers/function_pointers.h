6b2ee2396a4a65f8c6c1ba98bd488cb3626e8cd5
Bor3y9 <yousef161247@eng.psu.edu.eg>
2023-08-17 14:31:05 -0700

    0x0F-function_pointers

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,12 +1,9 @@
-#ifndef _FUNCTION_POINTERS_H_
-#define _FUNCTION_POINTERS_H_
-/**
- * File: main
- *
- * Desc: header file
-**/
-void print_name(char *name, void (*f)(char *));
-void array_iterator(int *array, size_t size, void (*action)(int));
+#ifndef HEADER_FILE
+#define HEADER_FILE
+#include <stddef.h>
+
 int int_index(int *array, int size, int (*cmp)(int));
+void array_iterator(int *array, size_t size, void (*action)(int));
+void print_name(char *name, void (*f)(char *));
 
 #endif
