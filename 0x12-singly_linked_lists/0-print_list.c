8cad99b4744b3b5ebd5224907d33c88bff3a645d
Bor3y9 <yousef161247@eng.psu.edu.eg>
2023-08-25 00:30:08 +0300

    elements of linked list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a linked_list
+ * @h: a linked_list
+ * Return: a structure to a linked_list
+ **/
+
+size_t print_list(const list_t *h)
+{
+	int size = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[0] (nill)\n");
+		else
+			printf("[%i] %s\n", h->len, h->str);
+		h = h->next;
+		size++;
+	}
+
+	return (size);
+
+}
